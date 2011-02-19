9ddc7cc40debac8a5e105d23dcca4ea2e1da47af
roobs <reuben.fletchercostin@gmail.com>
2011-02-19 16:43:49 +1100

    added random soln sampler and example

--- bdd.py
+++ bdd.py
@@ -25,16 +25,38 @@ and the False sink has key 0.
 """
 
 BDD_X = dict(
-    n_args = 4,
     s = 9,
     dag = {8:(1,7,6), 7:(2,5,4), 6:(2,0,1), 5:(3,1,0), 4:(3,3,2), 3:(4,1,0), 2:(4,0,1), 1:(5,1,1), 0:(5,0,0)},
 )
 
 BDD_Y = dict(
-    n_args = 4,
     s = 9,
     dag = {8:(1,7,2), 7:(2,4,6), 6:(3,3,5), 5:(4,0,1), 4:(3,1,0), 3:(4,1,0), 2:(2,0,1), 1:(5,1,1), 0:(5,0,0)},
 )
+
+# hand-linearised from DAG on Knuth p76
+BDD_INDEP_SETS = dict(
+    s = 16,
+    dag = {
+        15:(1,14,13),
+        14:(2,12,11),
+        13:(2,10,0),
+        12:(3,9,8),
+        11:(3,9,0),
+        10:(3,7,6),
+        9:(4,5,4),
+        8:(4,5,0),
+        7:(4,2,3),
+        6:(4,2,0),
+        5:(5,1,2),
+        4:(5,1,0),
+        3:(5,2,0),
+        2:(6,1,0),
+        1:(7,1,1),
+        0:(7,0,0),
+    },
+)
+
 
 def bdd_root(bdd):
     # nb this implies convention that s=2 of a 1 bead BDD containing only True sink (Knuth p75)
@@ -81,7 +103,7 @@ def bdd_count_solutions(bdd, c = None):
         v_h = dag[h][0]
         # nb the exponential weights are required because we can jump from testing
         # a variable x_i to a variable x_j with j > i + 1. this skips the variables
-        # between i and j, so we mulitply the count by 2 for each one we skip..
+        # between i and j, so we multiply the count by 2 for each one we skip...
         c[k] = (2 ** (v_l - v_k - 1)) * c[l] + (2 ** (v_h - v_k - 1)) * c[h]
 
     # first variable tested, ie by the root bead, may not be the first variable
@@ -89,11 +111,50 @@ def bdd_count_solutions(bdd, c = None):
     v_root = dag[s - 1][0]
     return 2 ** (v_root - 1) * c[s - 1]
 
+def bdd_generate_random_solution(bdd, c, rand):
+    s = bdd['s']
+    dag = bdd['dag']
+    x = []
+    k = s - 1
+    prev_v = 0
+    while True:
+        v_k, l_k, h_k = dag[k]
+        # if we skip over testing variables, they do not matter, so set the bits randomly
+        for _ in xrange(v_k - prev_v - 1):
+            x.append(int(rand() < 0.5))
+        # terminate if we are in a sink bead
+        if (k == l_k):
+            if k == 1:
+                return x
+            else:
+                raise ValueError('there are no solutions')
+        prev_v = v_k
+        if (rand() * c[k]) < c[h_k]:
+            x.append(1)
+            k = h_k
+        else:
+            x.append(0)
+            k = l_k
+
+
+
+
+
 def main():
     print 'are BDDs x and y equal? %s' % bdd_equality(BDD_X, BDD_Y)
 
     print 'how many solutions does x have? %d' % bdd_count_solutions(BDD_X)
     print 'how many solutions does y have? %d' % bdd_count_solutions(BDD_Y)
+
+    c = {} # tabulate soln counts at each bead in here
+    n_indep_sets_solns = bdd_count_solutions(BDD_INDEP_SETS, c)
+    print 'how many solutions does indep_sets have? %d' % n_indep_sets_solns
+    assert n_indep_sets_solns == 18
+
+    import random
+    print bdd_generate_random_solution(BDD_INDEP_SETS, c, random.random)
+    print bdd_generate_random_solution(BDD_INDEP_SETS, c, random.random)
+    print bdd_generate_random_solution(BDD_INDEP_SETS, c, random.random)
 
 if __name__ == '__main__':
     main()
