12bb3937ad47b22824d93020f6507fbd91a0ee03
fcostin <reuben.fletchercostin@gmail.com>
2011-02-27 13:38:06 +1100

    plot random solns with pylab!

--- connection.py
+++ connection.py
@@ -15,6 +15,7 @@ get pretty ugly.
 
 import sys
 import heapq
+import numpy
 
 # define an ordering for the vertices by BFS from some root
 def order_vertices(vertices, edges, root = None):
@@ -219,7 +220,7 @@ def test():
     print 'making a %d by %d grid' % (n, n)
     vertices, edges = make_grid(n)
     # experiment: trying to fix roots
-    # central_root = (n/2, ) * 2 # this seems to work poorly
+    central_root = (n/2, ) * 2 # this seems to work poorly
     corner_root = (0, ) * 2
     vertex_order = order_vertices(vertices, edges, root = corner_root)
     edge_order = order_edges(vertices, edges, vertex_order)
@@ -246,6 +247,46 @@ def test():
     print ''
     print 'output (reduced) contains %d beads' % len(beads)
     print ''
+
+    import pylab
+    plots_across = 20
+    plots_down = 14
+    n_plots = plots_across * plots_down
+    pylab.figure()
+    for plot, soln in enumerate(gen_random_solutions(beads, how_many = n_plots)):
+        bmp = numpy.zeros((2 * n + 1, ) * 2, dtype = numpy.int)
+        def carve_edge(edge_i):
+            (u_i, v_i) = edge_order[edge_i]
+            (u_x, u_y) = vertex_order[u_i]
+            (v_x, v_y) = vertex_order[v_i]
+            bmp[2 * u_x + 1, 2 * u_y + 1] = 1
+            bmp[2 * v_x + 1, 2 * v_y + 1] = 1
+            if u_x == v_x and u_y == v_y + 1:
+                bmp[2 * u_x + 1, 2 * v_y + 2] = 1
+            elif u_x == v_x and u_y + 1 == v_y:
+                bmp[2 * u_x + 1, 2 * u_y + 2] = 1
+            elif u_x == v_x + 1 and u_y == v_y:
+                bmp[2 * v_x + 2, 2 * u_y + 1] = 1
+            elif u_x + 1 == v_x and u_y == v_y:
+                bmp[2 * u_x + 2, 2 * u_y + 1] = 1
+        for i, include_edge in enumerate(soln):
+            if include_edge:
+                carve_edge(i)
+        pylab.subplot(
+            plots_down,
+            plots_across,
+            plot + 1
+        )
+        pylab.imshow(
+            1 - bmp,
+            interpolation = 'nearest',
+            cmap = pylab.cm.Greys,
+        )
+        pylab.xticks([])
+        pylab.yticks([])
+        print soln
+    pylab.show()
+
     if len(beads) <= 1000:
         dump_graph(beads, 'tree.gv')
         sys.exit(0)
@@ -281,7 +322,8 @@ def dump_graph(beads, file_name):
             else:
                 raise ValueError('bad bead : %s' % str(bead))
             if is_sink:
-                label = 'T' if index else '&#8869;' # html entity (decimal) for unicode uptack (upside down T)
+                # n.b. html entity (decimal) for unicode uptack (upside down T)
+                label = 'T' if index else '&#8869;'
                 write_line('\t\t"%d" [label="%s", shape = box];' % (index, label))
             else:
                 write_line('\t\t"%d" [label="%d", shape = circle];' % (index, value))
@@ -349,7 +391,37 @@ def reduce_beads(beads):
     walk(root_key)
     print 'reduce : finished, returning'
     #XXX TODO fix keying? sigh.
-    return reduced_beads
+    return rekey_monotone(reduced_beads)
+
+def rekey_monotone(beads):
+    r = {}
+    for i, j in enumerate(sorted(beads.keys())):
+        r[j] = i
+    relabled_beads = {}
+    def relabel(key):
+        v, l, h = beads[key]
+        relabled_beads[r[key]] = (v, r[l], r[h])
+    for key in beads:
+        relabel(key)
+    return relabled_beads
+
+
+def gen_random_solutions(beads, how_many):
+    from bdd import bdd_count_solutions, bdd_generate_random_solution
+    bdd_beads = {
+        's' : len(beads),
+        'dag' : beads,
+    }
+    c = {}
+    n_solns = bdd_count_solutions(bdd_beads, c)
+    print 'number of solutions : %d' % n_solns
+    print 'here are a few random ones:'
+    for _ in xrange(how_many):
+        yield bdd_generate_random_solution(
+            bdd_beads,
+            c,
+            rand = numpy.random.rand
+        )
 
 if __name__ == '__main__':
     test()
